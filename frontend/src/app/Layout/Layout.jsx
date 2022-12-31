178a17f82ed39c2e32d13d73ce9f638ff1decccc
Talha Balaj <talhabalaj@hotmail.com>
2022-12-31 16:57:39 +0500

    add warning

--- frontend/src/app/Layout/Layout.jsx
+++ frontend/src/app/Layout/Layout.jsx
@@ -4,9 +4,16 @@ import { Footer } from './Footer'
 import { Ribbon } from './Ribbon/Ribbon'
 import { Outlet } from 'react-router-dom'
 
+const Warning = () => {
+  return <div className="warning">
+    This site is not KFC and has no affiliation with KFC, this site was made as a learning project. You can't order anything here. 
+  </div>
+}
+
 export const Layout = () => {
   return (
     <div className="layout">
+      <Warning />
       <Ribbon />
       <Nav />
       <div className="content">
